10cbd07af29b8e29601f129e635bcdac6e400d54
Naman Singhal <naman1079.cse18@chitkara.edu.in>
2021-06-07 11:19:43 +0530

    first commit

---
+++ pizzahouse/app/Http/Controllers/PizzaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Pizza;
+
+class PizzaController extends Controller
+{
+
+  public function index() {
+
+    $pizzas = Pizza::latest()->get();      
+
+    return view('pizzas.index', [
+      'pizzas' => $pizzas,
+    ]);
+  }
+
+  public function show($id) {
+    
+    $pizza = Pizza::findOrFail($id);
+
+    return view('pizzas.show', ['pizza' => $pizza]);
+  }
+
+  public function create() {
+    return view('pizzas.create');
+  }
+
+}
